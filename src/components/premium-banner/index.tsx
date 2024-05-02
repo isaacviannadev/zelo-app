06a0dc978c6f2a1d9caffcde05f4718b93857c2f
isaac <isaacsvianna@gmail.com>
2024-05-02 20:03:53 +0100

    chore: premium banner v1

--- src/components/premium-banner/index.tsx
+++ src/components/premium-banner/index.tsx
@@ -2,8 +2,10 @@ import { Card } from '@/components/ui/card';
 
 export default function PremiumBanner() {
   return (
-    <Card className="bg-zinc-700 bg-[url('/img/hero-pattern.svg')] ">
-      <h1>Premium</h1>
+    <Card className='flex items-end justify-end bg-hero-premium bg-cover bg-center h-96 relative overflow-hidden '>
+      <div className='bg-brand-600/90 absolute -bottom-20 -right-14 w-80 rounded-full h-40 -rotate-[45deg]'></div>
+      <div className='bg-white/80 absolute -bottom-24 -right-11 w-80 rounded-full h-40 -rotate-[25deg]'></div>
+      <h1 className='p-4 z-0 text-2xl'>Premium</h1>
     </Card>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,6 +80,9 @@ const config = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      backgroundImage: {
+        'hero-premium': "url('/premium-banner.png')",
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
