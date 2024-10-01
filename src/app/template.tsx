9b80c2dfcf09ea7125056bf0379d9490fba50442
isaac <isaacsvianna@gmail.com>
2024-10-01 10:37:16 +0100

    feat: sync

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,14 +1,7 @@
-import Header from '@/components/header';
-
 type TemplateProps = {
   children: React.ReactNode;
 };
 
 export default function Template({ children }: TemplateProps) {
-  return (
-    <>
-      <Header user={null} />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
